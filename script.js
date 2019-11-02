5ea9121605141b2ac5ca343ff2c1b987b7e88e25
etefia60994 <etefia60994@gmail.com>
2019-11-02 14:51:14 -0500

    Finished

--- script.js
+++ script.js
@@ -1,32 +1,82 @@
 // Write your JavaScript code here!
+
 let submitButton = document.getElementById("formSubmit");
-let pilotNameInput = document.querySelector("input[name=pilotName]");
-let copilotNameInput = document.querySelector("input[name=copilotName]");
-let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
-let cargoMassInput = document.querySelector("input[name=cargoMass]");
+let pilotName = document.querySelector("input[name=pilotName]");
+let copilotName = document.querySelector("input[name=copilotName]");
+let fuelLevel = document.querySelector("input[name=fuelLevel]");
+let cargoMass = document.querySelector("input[name=cargoMass]");
+let faultyItems = document.getElementById("faultyItems")
+let pilotStatus = document.getElementById("pilotStatus");
+let copilotStatus = document.getElementById("copilotStatus");
+let fuelStatus = document.getElementById("fuelStatus");
+let cargoStatus = document.getElementById("cargoStatus");
+let launchStatus = document.getElementById("launchStatus")
+// let launchStatusCheck = true;
+window.addEventListener("load", function () {
+   let form = document.querySelector("form");
+   fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
 
+         response.json().then(function(json) {
+       const div = document.getElementById('missionTarget');
+       // Add HTML that includes the JSON Data
+            div.innerHTML = `
+                           <h2>Mission Destination</h2>
+                           <ol>
+                              <li>Name: ${json[0].name}</li>
+                              <li>Diameter: ${json[0].diameter}</li>
+                              <li>Star: ${json[0].star}</li>
+                              <li>Distance from Earth: ${json[0].distance}</li>
+                              <li>Number of Moons: ${json[0].moons}</li>
+                           </ol> 
+                           <img src="${json[0].image}">
+                         `;         
+             console.log(json);
+         });
+      });
+   form.addEventListener("submit", function () {
+      if (isNaN(fuelLevel.value) === true || isNaN(cargoMass.value) === true) {
+         alert("Invalid Data, Fuel Level and Cargo Mass Must Be Numbers");
+         event.preventDefault();
+         
+      }
+      if (pilotName.value === "" || copilotName.value === "" || fuelLevel.value === "" || cargoMass.value === "") {
+         alert("Invalid Data, Must Fill Out All Fields");
+         event.preventDefault();
+         
+      }
+      if (isNaN(pilotName.value) === false || isNaN(copilotName.value) === false) {
+         alert("Invalid Data, Pilot Names must not contain numbers");
+         event.preventDefault();
+      }
+      if (fuelLevel.value < 10000) {
+         faultyItems.style.visibility = "visible";
+         pilotStatus.innerHTML = ` Pilot ${pilotName.value} is ready`;
+         copilotStatus.innerHTML = `Co-Pilot ${copilotName.value} is ready`;
+         fuelStatus.innerHTML = "Not enough fuel for the journey";
+         launchStatus.innerHTML = "Shuttle is not ready for launch";
+         document.getElementById('launchStatus').style.color = 'red';
+         event.preventDefault();
+         
 
-submitButton.addEventListener("click",function isBlank(){
-   if (pilotNameInput.value === ""|| copilotNameInput.value === "" || fuelLevelInput.value === ""|| cargoMassInput.value === ""){
-      alert("Invalid Data, Must Fill Out All Fields");
-      event.preventDefault();
-   }
+      }
+
+      if (cargoMass.value > 10000) {
+         faultyItems.style.visibility = "visible";
+         pilotStatus = pilotStatus += pilotName
+         copilotStatus = copilotStatus += copilotName
+         cargoStatus.innerHTML = "Too much mass for the shuttle to launch";
+         document.getElementById('launchStatus').innerHTML = "Shuttle is not ready for launch";
+         document.getElementById('launchStatus').style.color = 'red';
+         event.preventDefault();
+      }
+
+      if (launchStatusCheck === true) {
+         launchStatus.innerHTML = "Shuttle is ready for launch"
+         pilotStatus = pilotStatus += pilotName
+         copilotStatus = copilotStatus += copilotName
+         // launchStatus.attr("id", "launchStatusValid");
+      }
+      
+   });
 });
-submitButton.addEventListener("click",function isNumber(){
-   if (isNaN(fuelLevelInput.value)  === true  || isNaN(cargoMassInput.value) === true ){
-      alert("Invalid Data, Fuel Level and Cargo Mass Must Be Numbers");
-      event.preventDefault();
-   }
-});
 
-/* This block of code shows how to format the HTML once you fetch some planetary JSON!
-<h2>Mission Destination</h2>
-<ol>
-   <li>Name: ${}</li>
-   <li>Diameter: ${}</li>
-   <li>Star: ${}</li>
-   <li>Distance from Earth: ${}</li>
-   <li>Number of Moons: ${}</li>
-</ol>
-<img src="${}">
-*/
