657042e30e957d4e189b647b21050ea643f10605
etefia60994 <etefia60994@gmail.com>
2019-11-02 07:39:19 -0500

    Invalid Data

--- script.js
+++ script.js
@@ -1,21 +1,12 @@
 // Write your JavaScript code here!
-let userData = document.getElementsByName("formStuff")
-function dataCheck (userData){
-  if(userData === ""){
-     alert("Invalid Data");
-  };
-   return data;
-}
-window.addEventListener("load", function() {
-   let form = document.querySelector("form");
-   form.addEventListener("formSubmit", function(event) {
-      dataCheck(userData);
-      if (data = false){
-         // preventDefault();
-         alert("Invalid Data");
-      }
-   });
-});
+console.log("Runnning?")
+let submitButton = document.getElementById("formSubmit");
+let userInput = document.getElementsByTagName("input");
+submitButton.addEventListener("click",function(){
+   if (userInput == ""){
+      alert("Invalid Data")
+   }
+})
 
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
